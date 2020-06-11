7d197287ee01ce98c91ed06f56f6de1ca2e1bc44
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-11 00:50:11 -0400

    configurando os erros no Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -18,6 +18,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     const inputRef = useRef<HTMLInputElement>(null);
     const [isFocused, setIsFocused] = useState(false);
     const [isFilled, setIsFilled] = useState(false);
+
     const { fieldName, defaultValue, error, registerField } = useField(name);
 
     const handleInputFocus = useCallback(() => {
@@ -48,6 +49,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
                 ref={inputRef}
                 {...rest}
             />
+            {error}
         </Container>
     );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,36 +1,46 @@
-import React, {useCallback} from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Container, Content, Background } from './styles';
+import { FormHandles } from '@unform/core';
 import logo from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import getValidationErrors from '../../utils/getValidationErrors';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-    const handleSubmit = useCallback(async  (data: object) => {
+    const formRef = useRef<FormHandles>(null);
+
+    const handleSubmit = useCallback(async (data: object) => {
         try {
+            formRef.current?.setErrors({});
             const shema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatório'),
                 email: Yup.string().required('Email obrigatório').email(),
-                password: Yup.string()
-                    .min(6, 'No minimo senha com 6 caracteres')
+                password: Yup.string().min(
+                    6,
+                    'No minimo senha com 6 caracteres',
+                ),
             });
             await shema.validate(data, {
-                abortEarly: false
+                abortEarly: false,
             });
         } catch (err) {
-            console.log(err);
+            const errors = getValidationErrors(err);
+            formRef.current?.setErrors(errors);
         }
-    }, [])
+    }, []);
 
     return (
         <Container>
             <Background />
             <Content>
                 <img src={logo} alt="GoBarver" />
-                <Form onSubmit={handleSubmit}>
+
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Faça seu cadastro</h1>
                     <Input name="name" icon={FiUser} placeholder="Nome" />
                     <Input
