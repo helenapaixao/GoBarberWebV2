32d0c92ccd47f7a3d7af7241d58b6b6ef9c896d3
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-11 00:25:14 -0400

    validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data: object): void {
-        console.log(data);
+    const handleSubmit = useCallback(async  (data: object) => {
+        try {
+            const shema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('Email obrigatório').email(),
+                password: Yup.string()
+                    .min(6, 'No minimo senha com 6 caracteres')
+            });
+            await shema.validate(data, {
+                abortEarly: false
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, [])
 
-    }
     return (
         <Container>
             <Background />
             <Content>
                 <img src={logo} alt="GoBarver" />
-                <Form  onSubmit={handleSubmit}>
+                <Form onSubmit={handleSubmit}>
                     <h1>Faça seu cadastro</h1>
                     <Input name="name" icon={FiUser} placeholder="Nome" />
                     <Input
