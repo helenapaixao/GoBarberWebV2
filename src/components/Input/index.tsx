1bd742d079bf688adc9e36719e3fabb7ddb29246
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-10 22:32:40 -0400

    add unform no projeto

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,30 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { Container } from './styles';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
     icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-    <Container>
-        { Icon && <Icon size={20} />}
-        <input {...rest} />
-    </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+    const inputRef = useRef(null);
+    const { fieldName, defaultValue, error, registerField } = useField(name);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value',
+        });
+    }, [fieldName, registerField]);
+    return (
+        <Container>
+            {Icon && <Icon size={20} />}
+            <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+        </Container>
+    );
+};
 
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,29 +2,45 @@ import React from 'react';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-    <Container>
-            <Background/>
-        <Content>
-            <img src={logo} alt="GoBarver" />
-            <form>
-                <h1>Faça seu cadastro</h1>
-                <Input  name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="forgot">
-                <FiArrowLeft />
-                Voltar para Logon
-            </a>
-        </Content>
-    
-    </Container>
-);
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object): void {
+        console.log(data);
+
+    }
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logo} alt="GoBarver" />
+                <Form  onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input
+                        name="email"
+                        icon={FiMail}
+                        type="email"
+                        placeholder="E-mail"
+                    />
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="forgot">
+                    <FiArrowLeft />
+                    Voltar para Logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
