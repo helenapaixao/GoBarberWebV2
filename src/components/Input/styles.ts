b40289fc752a4601ca4fa246dfbf5a16c89b41ab
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-10 23:05:00 -0400

    adicionando usabilidade do input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,31 +1,46 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+    isFocused: boolean;
+    isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
     background: #232129;
     color: #f4ede8;
     border-radius: 10px;
     border: 2px solid #232129;
     padding: 16px;
     width: 100%;
-    align-items:center;
+    align-items: center;
     color: #666360;
 
     & + div {
-            margin-top: 8px;
-        }
+        margin-top: 8px;
+    }
+
+    ${props =>
+        props.isFocused &&
+        css`
+            color: #ff9000;
+            border-color: #ff9000;
+        `}
+
+    ${props =>
+        props.isFilled &&
+        css`
+            color: #ff9000;
+        `}
 
     input {
         background: transparent;
-        display:1;
-        border:0;
+        display: 1;
+        border: 0;
         color: #f4ede8;
-
 
         &::placeholder {
             color: #666360;
         }
-
-        
     }
 
     svg {
