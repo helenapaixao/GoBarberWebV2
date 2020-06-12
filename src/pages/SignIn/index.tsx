d642edd3b96170d209acd59a5ce332520a851926
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-12 04:14:50 -0400

    quase finalizando

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,7 +5,7 @@ import { FormHandles } from '@unform/core';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 import { useAuth } from '../../hooks/auth';
@@ -23,6 +23,7 @@ const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
     const { signIn } = useAuth();
     const { addToast } = useToast();
+    const history = useHistory();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
@@ -43,6 +44,7 @@ const SignIn: React.FC = () => {
                     email: data.email,
                     password: data.password,
                 });
+                history.push('/dashboard');
             } catch (err) {
                 if (err instanceof Yup.ValidationError) {
                     const errors = getValidationErrors(err);
@@ -58,7 +60,7 @@ const SignIn: React.FC = () => {
                 });
             }
         },
-        [signIn, addToast],
+        [signIn, addToast, history],
     );
 
     return (
