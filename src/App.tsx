56759255fa395e98398f3fdb405069e10b9e1a55
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-12 01:13:55 -0400

    criando hook de toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import SignIn from './pages/SignIn';
 //import SignUp from './pages/SignUp';
 import GlobalStyle from '../src/styles/global';
 
-import ToastContainer from '../src/components/ToastContainer'
-import { AuthProvider } from './hooks/AuthContext';
+
+import AppProvider from './hooks/index';
 
 const App: React.FC = () => (
     <>
-        <AuthProvider>
+        <AppProvider>
             <SignIn />
-        </AuthProvider>
-        
-        <ToastContainer/>
+        </AppProvider>
+
+    
         <GlobalStyle />
     </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -7,8 +7,8 @@ import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
 import getValidationErrors from '../../utils/getValidationErrors';
-import { useAuth } from '../../hooks/AuthContext';
-
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 
 import Input from '../../components/Input';
@@ -22,6 +22,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
     const { signIn } = useAuth();
+    const { addToast} = useToast();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
@@ -38,7 +39,7 @@ const SignIn: React.FC = () => {
                     abortEarly: false,
                 });
 
-                signIn({
+                await signIn({
                     email: data.email,
                     password: data.password,
                 });
@@ -48,11 +49,11 @@ const SignIn: React.FC = () => {
                     formRef.current?.setErrors(errors);
                 }
                
-                    //disparar um toast
+                    addToast();
                 
             }
         },
-        [signIn],
+        [signIn, addToast],
     );
 
     return (
