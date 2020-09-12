1a61df622a054d61177c68bc66a4765728d44713
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-12 19:37:19 -0400

    listagem de agendamentos: ok

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Container, Header, HeaderContent, Profile, Content, Schedule,Calendar,NextAppointment} from './styles';
+import React, { useState } from 'react';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    Calendar,
+    NextAppointment,
+    Section,
+    Appointment,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
     const { signOut, user } = useAuth();
+    const [selectedDate, setSelectedDate] = useState(new Date())
     console.log(user);
     return (
         <Container>
@@ -26,33 +38,61 @@ const Dashboard: React.FC = () => {
             </Header>
             <Content>
                 <Schedule>
-                <h1>Horários agendados</h1>
-                <p>
-                    <span>
-                        Hoje
-                    </span>
-                    <span>
-                        Hoje
-                    </span>
-                    <span>
-                        Hoje
-                    </span>
-                </p>
-                <NextAppointment>
-                    <strong>Atendimento a seguir</strong>
-                    <div>
-                        <img src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=06cb1598cc0323d116f30792141f147c8bd3ed28&v=4://github.com/account"/>
-                        <strong>
-                           Diego Fernandes 
-                        </strong>
-                        <span>
-                            <FiClock/>
-                            08:00
-                        </span>
-                    </div>
-                </NextAppointment>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Hoje</span>
+                        <span>Hoje</span>
+                    </p>
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=06cb1598cc0323d116f30792141f147c8bd3ed28&v=4://github.com/account" />
+                            <strong>Diego Fernandes</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+                    <Section>
+                        <strong>Manhã</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=06cb1598cc0323d116f30792141f147c8bd3ed28&v=4://github.com/account" />
+                                <strong>Diego Fernandes</strong>
+                            </div>
+                        </Appointment>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=06cb1598cc0323d116f30792141f147c8bd3ed28&v=4://github.com/account" />
+                                <strong>Diego Fernandes</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+                    <Section>
+                        <strong>Tarde</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=06cb1598cc0323d116f30792141f147c8bd3ed28&v=4://github.com/account" />
+                                <strong>Diego Fernandes</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
                 </Schedule>
-                <Calendar/>
+                <Calendar />
             </Content>
         </Container>
     );
